e6bd7b6a7c8902669d265b21f0c01496131377d3
Andrew Bruenger <ab8242@gmail.com>
2024-03-12 22:36:38 -0600

    Created question form

--- projectAndAlgosGroupProject/src/main/java/com/projectalgos/groupproject/models/Question.java
+++ projectAndAlgosGroupProject/src/main/java/com/projectalgos/groupproject/models/Question.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="questions")
 
 public class Question {
 	@Id
--- projectAndAlgosGroupProject/src/main/java/com/projectalgos/groupproject/models/Quiz.java
+++ projectAndAlgosGroupProject/src/main/java/com/projectalgos/groupproject/models/Quiz.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,10 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="quizzes")
 
 public class Quiz {
 	@Id
